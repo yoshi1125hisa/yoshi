3ee7c99127327470f9771708bd4e8dc7affe1200
Artem Yavorsky <aqson@me.com>
2019-05-08 19:26:59 +0300

    Update fullstack-babel template to use lazy-loaded Intro.

--- packages/create-yoshi-app/templates/fullstack/javascript/src/components/App/App.js
+++ packages/create-yoshi-app/templates/fullstack/javascript/src/components/App/App.js
@@ -3,19 +3,40 @@ import { translate } from 'react-i18next';
 import s from './App.scss';
 import PropTypes from 'prop-types';
 
+/* <-- To remove demo stuff just copy-paste:
+  \{?/\*\s?<--([\n\n]|.)*?-->\s?\*\/\}?
+  to your search input with RegExp enabled and remove everything matched.
+--> */
+
 class App extends React.Component {
   static propTypes = {
     t: PropTypes.func,
   };
+  state = {};
+
+  /* <-- Feel free to remove this lifecycle hook */
+  async componentDidMount() {
+    await import(
+      /* webpackIgnore: true */
+      // eslint-disable-next-line import/no-unresolved
+      'https://unpkg.com/yoshi-template-intro'
+    );
+    const { default: TemplateIntro } = window.TemplateIntro;
+    this.setState({ TemplateIntro });
+  } /* --> */
 
   render() {
     const { t } = this.props;
+    const { TemplateIntro } = this.state;
+
     return (
       <div className={s.root}>
-        <div className={s.header}>
-          <h2 data-testid="app-title">{t('app.title')}</h2>
-        </div>
-        <p className={s.intro}>{t('app.intro')}</p>
+        {/* <-- Feel free to remove h2 and TemplateIntro */}
+        <h2 className={s.title} data-testid="app-title">
+          {t('app.title')}
+        </h2>
+        {TemplateIntro && <TemplateIntro />}
+        {/* --> */}
       </div>
     );
   }
